b089d3bc7e198babad7f267394bad624f439c285
Пономарёв Александр <neulovimbiy@gmail.com>
2020-02-02 21:31:51 +0300

    create model person

---
+++ LabAiMinFunConsole/Model/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabAiMinFunConsole
+{
+    abstract class Person
+    {
+        private double [] _coordinate;
+        private double [] _coordinateValue;
+
+        public Person(double [] coordinate, double [] coordinateValue)
+        {
+            _coordinate = coordinate;
+            _coordinateValue = coordinateValue;
+        }
+
+        public double [] Coordinate
+        {
+            get
+            {
+                return _coordinate;
+            }
+
+            set
+            {
+                _coordinate = value;
+            }
+        }
+
+        public double [] CoordinateValue
+        {
+            get
+            {
+                return _coordinateValue;
+            }
+
+            set
+            {
+                _coordinateValue = value;
+            }
+        }
+    }
+}
