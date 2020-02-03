6070baac37328ac670a94eac631b232be85bed77
Пономарёв Александр <neulovimbiy@gmail.com>
2020-02-03 22:57:25 +0300

    created start program

---
+++ LabAiMinFunConsole/Controller/Program.cs
@@ -0,0 +1,15 @@
+﻿using LabAiMinFunConsole.Model;
+using LabAiMinFunConsole.View;
+using System;
+
+namespace LabAiMinFunConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EvolutionCreator evolution = new EvolutionCreator();
+            evolution.StartEvolution();
+        }
+    }
+}
