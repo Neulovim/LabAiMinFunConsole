16ccc51ce9798a10d32efe97b33ae894b224c752
Пономарёв Александр <neulovimbiy@gmail.com>
2020-02-03 20:33:44 +0300

    created "child creator"

---
+++ LabAiMinFunConsole/View/ChildCreator.cs
@@ -0,0 +1,35 @@
+﻿using LabAiMinFunConsole.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabAiMinFunConsole.View
+{
+    class ChildCreator
+    {
+        Random random = new Random();
+        FunctionSumSquare function = new FunctionSumSquare();
+        public Child CreateChild(Father father)
+        {
+            double[] coordinates = GetChildCoordinates(father);
+            Child child = new Child(coordinates, GetChildCoordinatesValue(coordinates), father);
+            return child;
+        }
+
+        private double[] GetChildCoordinates(Father father)
+        {
+            int count = father.Coordinates.Length;
+            double[] coordinates = new double[count];
+            for (int i = 0; i < count; i++)
+            {
+                coordinates[i] =  random.NextDouble() + father.Coordinates[i] - random.NextDouble();
+            }
+            return coordinates;
+        }
+
+        private double GetChildCoordinatesValue(double[] coordinates)
+        {
+            return function.GetFunction(coordinates);
+        }
+    }
+}
