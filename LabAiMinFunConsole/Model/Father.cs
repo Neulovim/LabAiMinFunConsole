bd72ae12fe9beb2889f7dcfa6c550886eea3d2d0
Пономарёв Александр <neulovimbiy@gmail.com>
2020-02-03 14:29:50 +0300

    created father model

---
+++ LabAiMinFunConsole/Model/Father.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabAiMinFunConsole.Model
+{
+    class Father : Person
+    {
+        public Father(double [] coordinates, double coordinatesValue) : base(coordinates, coordinatesValue)
+        {
+        }
+    }
+}
