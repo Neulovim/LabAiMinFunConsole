891a50c484a847a7cd9acd9bbc2d9f1444d87649
Пономарёв Александр <neulovimbiy@gmail.com>
2020-02-04 23:31:58 +0300

    created class sorter for list (persons and for him subclasses)

---
+++ LabAiMinFunConsole/View/Sorter.cs
@@ -0,0 +1,19 @@
+﻿using LabAiMinFunConsole.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LabAiMinFunConsole.View
+{
+    static class Sorter<T> where T: Person
+    {
+        public static List<T> GetSortedList(List<T> people)
+        {
+            IOrderedEnumerable<T> peopleSorted = from person in people
+                                                 orderby person.CoordinatesValue
+                                                 select person;
+            return peopleSorted.ToList();
+        }
+    }
+}
