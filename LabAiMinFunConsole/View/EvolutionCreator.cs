e9e2e19e89cf0d3382eaa2280e94b9ea2e64ee19
Пономарёв Александр <neulovimbiy@gmail.com>
2020-02-04 23:34:10 +0300

    changed sorted list code (replaced by methods)

--- LabAiMinFunConsole/View/EvolutionCreator.cs
+++ LabAiMinFunConsole/View/EvolutionCreator.cs
@@ -9,20 +9,21 @@ namespace LabAiMinFunConsole.View
     class EvolutionCreator
     {
         private const int N = 5;
-        private const double ALPHA = 0.0001;
+        private const double ALPHA = 0.00000001;
         private int iteration = 1;
 
         public void StartEvolution()
         {
             List<Father> fathers = GetFathersFirstDistribution();
-            IOrderedEnumerable<Father> fathersSorted;
             
-           
+            double sumFathersOld;
+            double sumFathersYung;
+
             while (true)
             {
-                fathersSorted = from father in fathers
-                                orderby father.CoordinatesValue
-                                select father;
+                sumFathersOld = CalculateSumFathersCoordinateValue(fathers);
+
+
                 /*
                 if (Math.Abs(fathers[0].CoordinatesValue - fathers[3].CoordinatesValue) < ALPHA)
                 {
@@ -39,23 +40,33 @@ namespace LabAiMinFunConsole.View
                     }
                     Console.WriteLine(iterationWalkingOnTheSpot);
                 }
-                */
+                
                 if (Math.Abs(fathers[0].CoordinatesValue - fathers[3].CoordinatesValue) < ALPHA)
                 {
                     Console.WriteLine($"iteration: {iteration}, value: {fathers[0].CoordinatesValue}");
                     break;
                 }
-                /*
+                
                 Console.WriteLine("Sorted");
                 foreach (Father f in fathersSorted)
                     Console.WriteLine($"father c: {f.Coordinates[0]}, {f.Coordinates[1]}, v: c:{f.CoordinatesValue}");
                 */
-                List<Child> children = GetChildren(fathersSorted.ToList());
+                //List<Child> children = GetChildren(GetSortedList(fathers));
+                List<Child> children = GetChildren(Sorter<Father>.GetSortedList(fathers));
                 for (int i = 0; i < fathers.Count; i++)
                 {
                     fathers[i].Coordinates = children[i].Coordinates;
                     fathers[i].CoordinatesValue = children[i].CoordinatesValue;
                 }
+                sumFathersYung = CalculateSumFathersCoordinateValue(fathers);
+                fathers = Sorter<Father>.GetSortedList(fathers);
+                if (Math.Abs(sumFathersYung - sumFathersOld) < ALPHA)
+                {
+                    Console.Write($"iteration: {iteration}, value: " + String.Format("{0:0.##########################}", fathers[0].CoordinatesValue));
+                    
+                    break;
+                }
+
                 /*
                 Console.WriteLine("New");
                 foreach (Father f in fathers)
@@ -64,6 +75,16 @@ namespace LabAiMinFunConsole.View
                 
                 iteration += 1;
             }
+        }
+
+        private double CalculateSumFathersCoordinateValue(List<Father> fathers)
+        {
+            double sum = 0;
+            foreach (Father father in fathers)
+            {
+                sum += father.CoordinatesValue;
+            }
+            return sum;
         }
 
         private List<Child> GetChildren(List<Father> fathers)
@@ -140,13 +161,15 @@ namespace LabAiMinFunConsole.View
         private int GetDimension()
         {
             Console.WriteLine("Enter dimension:");
-            return Convert.ToInt32(Console.ReadLine());
+            int dimension = Convert.ToInt32(Console.ReadLine());
+            return dimension > 0 ? dimension : dimension == 0 ? 1 : -dimension;
         }
 
         private int GetCountFathers()
         {
             Console.WriteLine("Enter count father:");
-            return Convert.ToInt32(Console.ReadLine());
+            int count = Convert.ToInt32(Console.ReadLine());
+            return count > 0 ? count : count == 0 ? 1 : -count;
         }
 
         private int[] ToDistributeEvenFathers(int countFather)
