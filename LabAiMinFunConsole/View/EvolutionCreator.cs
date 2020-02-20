c7003803835686bc31ef71743052687eb1928d65
Пономарёв Александр <neulovimbiy@gmail.com>
2020-02-20 10:08:34 +0300

    deleted comments, changed stop program from sum to time

--- LabAiMinFunConsole/View/EvolutionCreator.cs
+++ LabAiMinFunConsole/View/EvolutionCreator.cs
@@ -3,13 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace LabAiMinFunConsole.View
 {
     class EvolutionCreator
     {
-        
-        private const double ALPHA = 0.00000001;
+        private readonly int _timeStopMilliseconds = 1500;
+        private double alpha = 0.00000001;//0.00000001;
         private int iteration = 1;
 
         public void StartEvolution()
@@ -20,67 +21,49 @@ namespace LabAiMinFunConsole.View
             
             double sumFathersOld;
             double sumFathersYung;
+            Task t = Task.Run(() => {
+                while (true)
+                {
+                    //sumFathersOld = CalculateSumFathersCoordinateValue(fathers);
+                    List<Child> children = childCreator.Create(Sorter<Father>.GetSortedList(fathers));
+                    for (int i = 0; i < fathers.Count; i++)
+                    {
+                        fathers[i].Coordinates = children[i].Coordinates;
+                        fathers[i].CoordinatesValue = children[i].CoordinatesValue;
+                    }
+                    //sumFathersYung = CalculateSumFathersCoordinateValue(fathers);
+                    fathers = Sorter<Father>.GetSortedList(fathers);
+                    /*
+                    if (Math.Abs(sumFathersYung - sumFathersOld) < alpha)
+                    {
+                        Console.Write($"iteration: {iteration}, value: " + String.Format("{0:0.##########################}", fathers[0].CoordinatesValue));
 
-            while (true)
+                        break;
+                    }
+                    */
+                    //fatherCreator.Radius = iteration;
+                    //childCreator.Radius = iteration; //1 / Math.Pow(1000, iteration);
+                    //alpha = alpha / iteration;
+                    //fatherCreator.Radius = alpha / iteration;
+                    childCreator.Radius -= alpha / (iteration * 100);
+                    iteration += 1;
+                }
+            });
+            TimeSpan ts = TimeSpan.FromMilliseconds(_timeStopMilliseconds);
+            if (!t.Wait(ts))
             {
-                sumFathersOld = CalculateSumFathersCoordinateValue(fathers);
-
-
-                /*
-                if (Math.Abs(fathers[0].CoordinatesValue - fathers[3].CoordinatesValue) < ALPHA)
+                foreach (var coordinate in fathers[0].Coordinates)
                 {
-                    min = Math.Abs(fathers[0].CoordinatesValue - fathers[3].CoordinatesValue);
-                    if (min < ALPHA && tmp < ALPHA)
-                    {
-                        
-                        iterationWalkingOnTheSpot += 1;
-                        Console.WriteLine($"iteration: {iteration}, value: {fathers[0].CoordinatesValue}");                        
-                    }
-                    else
-                    {
-                        iterationWalkingOnTheSpot = 0;
-                    }
-                    Console.WriteLine(iterationWalkingOnTheSpot);
+                    Console.WriteLine($"coordinate: {coordinate}");
                 }
                 
-                if (Math.Abs(fathers[0].CoordinatesValue - fathers[3].CoordinatesValue) < ALPHA)
-                {
-                    Console.WriteLine($"iteration: {iteration}, value: {fathers[0].CoordinatesValue}");
-                    break;
-                }
-                
-                Console.WriteLine("Sorted");
-                foreach (Father f in fathersSorted)
-                    Console.WriteLine($"father c: {f.Coordinates[0]}, {f.Coordinates[1]}, v: c:{f.CoordinatesValue}");
-                */
-                //List<Child> children = GetChildren(GetSortedList(fathers));
-                List<Child> children = childCreator.Create(Sorter<Father>.GetSortedList(fathers));
-                for (int i = 0; i < fathers.Count; i++)
-                {
-                    fathers[i].Coordinates = children[i].Coordinates;
-                    fathers[i].CoordinatesValue = children[i].CoordinatesValue;
-                }
-                sumFathersYung = CalculateSumFathersCoordinateValue(fathers);
-                fathers = Sorter<Father>.GetSortedList(fathers);
-                if (Math.Abs(sumFathersYung - sumFathersOld) < ALPHA)
-                {
-                    Console.Write($"iteration: {iteration}, value: " + String.Format("{0:0.##########################}", fathers[0].CoordinatesValue));
-                    
-                    break;
-                }
-
-                /*
-                Console.WriteLine("New");
-                foreach (Father f in fathers)
-                    Console.WriteLine($"father c: {f.Coordinates[0]}, {f.Coordinates[1]}, v: c:{f.CoordinatesValue}");
-                */
-                //fatherCreator.Radius = 1 / Math.Pow(1000, iteration);
-                //childCreator.Radius = 1 / Math.Pow(1000, iteration);
-                
-                iteration += 1;
+                Console.WriteLine($"iteration: {iteration}, value: " + String.Format("{0:0.##########################}", fathers[0].CoordinatesValue));
+                Console.WriteLine($"time (milliseconds): {_timeStopMilliseconds}");
             }
+                
+            
         }
-
+        /*
         private double CalculateSumFathersCoordinateValue(List<Father> fathers)
         {
             double sum = 0;
@@ -90,21 +73,12 @@ namespace LabAiMinFunConsole.View
             }
             return sum;
         }
-
-        
-
+        */
         private int GetDimension()
         {
             Console.WriteLine("Enter dimension:");
             int dimension = Convert.ToInt32(Console.ReadLine());
             return dimension > 0 ? dimension : dimension == 0 ? 1 : -dimension;
         }
-
-        
-
-        
-
-
-
     }
 }
